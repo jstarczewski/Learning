6333224bb2cbdf449f0179d88c5ceb3e045c154c
jstarczewski <jan.a.starczewski@gmail.com>
2018-08-05 19:53:37 +0200

    Candle challange compleated

--- HackRank/Candle.java
+++ HackRank/Candle.java
@@ -2,23 +2,23 @@ import java.util.Random;
 public class Candle {
 
 	static int maxCandle(int[] ar) {
-		int max = 0;
-		int amount = 1;
+		int max = ar[0];
+		int amount = 0;
 		for (int i = 0; i<ar.length; i++) {
-			if (ar[i] == max) {
-				amount++;
-			}
 			if (ar[i] > max) {
 				max = ar[i];
 				amount = 1;
+			}	
+			else if (ar[i] == max) {
+				amount++;
 			}
-			
+		
 		}
 		return amount;
 	}
 
 	public static void main(String[] args) {
-		int[] ar = {11, 11, 13, 9, 10};
+		int[] ar = {1, 1, 1, 1, 1};
 		Random r = new Random();
 	//	for (int i = 0; i<ar.length; i++) {
 	//		ar[i] = r.nextInt(10);
