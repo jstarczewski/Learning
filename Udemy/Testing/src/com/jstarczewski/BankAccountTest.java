369e2d3aa0925266ff8674897f7999a6885bdcfd
jstarczewski <jan.a.starczewski@gmail.com>
2018-08-09 20:37:11 +0200

    More on testing

--- Udemy/Testing/src/com/jstarczewski/BankAccountTest.java
+++ Udemy/Testing/src/com/jstarczewski/BankAccountTest.java
@@ -4,31 +4,46 @@ import static org.junit.Assert.*;
 
 public class BankAccountTest {
 
+    @org.junit.BeforeClass
+    public static void setUpClass() {
+        System.out.println("Only once");
+    }
+
+    private BankAccount bankAccount;
+
+    @org.junit.Before
+    public void setUp() {
+        bankAccount = new BankAccount("Jan", "Star", 100);
+        System.out.println("new bank account");
+    }
+
+
     @org.junit.Test
     public void deposite() {
-        BankAccount bankAccount = new BankAccount("Jan", "Star", 100);
         double balance = bankAccount.deposite(100, true);
         assertEquals(200, balance, 0);
     }
 
     @org.junit.Test
     public void withdraw() {
-        BankAccount bankAccount = new BankAccount("Jan", "Star", 100);
         double balance = bankAccount.withdraw(100, true);
-        assertEquals(0,balance,0);
+        assertEquals(0, balance, 0);
     }
 
     @org.junit.Test
     public void getBalance_deposite() {
-        BankAccount bankAccount = new BankAccount("Jan", "Star", 100);
         bankAccount.deposite(100, true);
         assertEquals(200, bankAccount.getBalance(), 0);
     }
 
     @org.junit.Test
     public void getBalance_withdraw() {
-        BankAccount bankAccount = new BankAccount("Jan", "Star", 100);
         bankAccount.withdraw(100, true);
         assertEquals(0, bankAccount.getBalance(), 0);
+    }
+
+    @org.junit.AfterClass
+    public static void after() {
+        
     }
 }
