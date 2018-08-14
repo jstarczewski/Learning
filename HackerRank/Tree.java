2058cdb769a4f45656666254597b5a8e9f4ebfa0
jstarczewski <jan.a.starczewski@gmail.com>
2018-08-14 21:44:53 +0200

    Tree challange poor solution

--- HackerRank/Tree.java
+++ HackerRank/Tree.java
@@ -3,26 +3,21 @@ public class Tree {
 	static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
 		int appleCount =0;
 		int orangeCount = 0;
-		if ( (1<=s && 1<=t && 1 <= a && 1<= b && 1<=apples.length && 1<=oranges.length && 
-					s<= 1000000 && t<= 1000000 && a<=100000 && b<= 100000 && apples.length <= 100000 && oranges.length <= 1000000)
-			 &&(a < s && s < t && t < b) ) {
+		int max = 100000;
+		if (a<s && s<t && t< b && s>= 1 && t>= 1 && a >= 1 && b >= 1 && apples.length >= 1 && oranges.length >= 1 &&
+				s<= max && t<=max && a <= max && b <= max && apples.length <= max && oranges.length <= max ) {
 			for (int i = 0; i<apples.length; i++) {
-				if (apples[i] < 100000 && apples[i] > -100000 &&
-					       	s<=(a+apples[i]) && (a+apples[i])<=t)
+				if(apples[i] >= -max && apples[i] <= max && s<=(a+apples[i]) && (a+apples[i])<=t)
 					appleCount++;
 			}	
-			System.out.println(""+appleCount);
+			System.out.println(appleCount);
 			for (int i = 0; i<oranges.length; i++) {
-				if (oranges[i] < 100000 && oranges[i] > -100000 &&
-					       	s<=(b-oranges[i]) && (b+oranges[i])<=t)
+				if(oranges[i] >= -max && oranges[i] <= max && s<=(b+oranges[i]) && (b+oranges[i])<=t)
 					orangeCount++;
 			}
-			System.out.println(""+orangeCount);
-		}
-	
-	}
-	static void countAll(int s, int t, int a int b, int[] apples, int[] oranges) {
+			System.out.println(orangeCount);
 		
+		}
 	}
 
 
