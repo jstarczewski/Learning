dacd2dd5e5f1eb486fe68053cd11bb9f5c0bf9e8
jstarczewski <jan.a.starczewski@gmail.com>
2018-08-14 23:29:15 +0200

    Kangaroo challange finished

--- HackerRank/Kangaroo.java
+++ HackerRank/Kangaroo.java
@@ -1,7 +1,17 @@
 public class Kangaroo {
 	
+	static String kangaroo(int x1, int v1, int x2, int v2) {
+		if ((0<= x1 && x1<x2 && x2<= 10000) && (1<= v1 && v1 <= 10000 && 1<= v2 && v2 <= 10000)  && (v2<v1 && (x2-x1)%(v2-v1)==0))
+			return "YES";
+		else
+			return "NO";
+	}
+
 	public static void main(String[] args) {
-		System.out.println("ELo");
+		System.out.println(kangaroo(Integer.valueOf(args[0]), Integer.valueOf(args[1]), Integer.valueOf(args[2]),Integer.valueOf(args[3])));
+
+
+//		8, 4 | 11, 6 | 14, 8, 
 	}
 
 }
