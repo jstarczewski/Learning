e30f9761a6aa266fb7c9647b30fa22fdd7bcda41
jstarczewski <jan.a.starczewski@gmail.com>
2018-08-17 18:53:25 +0200

    Chocolate right answer

--- HackerRank/Chocolate.java
+++ HackerRank/Chocolate.java
@@ -5,9 +5,16 @@ public class Chocolate {
 	static int birthday(List<Integer> s, int d, int m) {
 		int count = 0;
 		int ways = 0;
-	    for (int i = m; i<s.size(); i++) {
+		if (s.size()>100 || 1 > d || 1 > m || d > 31 || m > 12)
+			return -1;
+		if (s.size()==1)
+			if (s.get(0)==d)
+				return 1;
+	    for (int i = m-1; i<s.size(); i++) {
 	    	count = 0;
 	    	for (int j = 0; j<m; j++) {
+	    	    if (s.get(i-j) > 5 || s.get(i-1) < 1)
+	    	    	return -1;
 	    		count+=s.get(i-j);
 			}
 			if (count==d)
@@ -19,12 +26,12 @@ public class Chocolate {
 
 	public static void main(String[] args) {
 		List<Integer> list = new ArrayList<>();
-		list.add(1);
-		list.add(2);
-		list.add(1);
-		list.add(3);
-		list.add(2);
-		System.out.println(" ways " + birthday(list, 3, 2));
+		list.add(4);
+	//	list.add(2);
+	//	list.add(1);
+	//	list.add(3);
+	//	list.add(2);
+		System.out.println(" ways " + birthday(list, 4, 1));
 
 	
 	}
