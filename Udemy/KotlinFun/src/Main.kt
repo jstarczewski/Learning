cc34af5269ca2aeebb25e1ee1fe47c8b1900cbc5
jstarczewski <jan.a.starczewski@gmail.com>
2018-10-01 10:14:52 +0200

    Fun in kotlin

--- Udemy/KotlinFun/src/Main.kt
+++ Udemy/KotlinFun/src/Main.kt
@@ -3,18 +3,37 @@ import java.util.*
 fun main(args: Array<String>) {
     //println(if (Integer.valueOf(args[0]) > 12) "Good morning Kotlin" else "Good night Kotlin")
     feedTheFish()
+    println(fishDayFood("jebacpsycmd"))
 }
+
 fun feedTheFish() {
 
     val day = randomDay()
     val food = randomFood()
     println("Today is $day and the fish eat $food")
 }
-fun randomDay() : String {
+
+fun randomDay(): String {
     val days = listOf("Monday", "Teusday", "Wednesday", "Czwartek", "Piatek", "Sobota", "Sontag")
     return days[(Random().nextInt(days.size))]
 }
-fun randomFood() : String {
-    val foods = listOf("Kebab", "Pizza", "Twaróg")
+
+fun randomFood(): String {
+    val foods = listOf("Kebab","Pizza", "Twaróg")
     return foods[(Random().nextInt(foods.size))]
+}
+
+fun fishDayFood(day: String): String {
+
+    return when (day) {
+        "Monday" -> "dsbraitak"
+        "Teusday" -> "policja"
+        "Wednesday" -> "elo"
+        "Czwartek" -> "xd"
+        "Piatek" -> "sx"
+        "Sobota" -> "jebbac"
+        "Sontag" -> "elo"
+        else -> "Twoja stara"
+    }
+
 }
