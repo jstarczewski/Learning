0706016def370aaae1e5421eebeaa12d09128562
jstarczewski <jan.a.starczewski@gmail.com>
2018-10-04 14:58:35 +0200

    Kotlin fun:

---
+++ DotnetCLI/MainProgram/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MainProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stara Stevena");
+		string elo = Console.ReadLine(); 	    
+            Console.WriteLine("Stara Stevena" + elo);
+        }
+    }
+}
