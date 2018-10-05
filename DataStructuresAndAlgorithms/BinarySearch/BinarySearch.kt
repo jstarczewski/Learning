3c59ca506000080d8c16fdf26d963a942d3d73cf
jstarczewski <jan.a.starczewski@gmail.com>
2018-10-05 22:03:25 +0200

    Binary search right implementation

--- DataStructuresAndAlgorithms/BinarySearch/BinarySearch.kt
+++ DataStructuresAndAlgorithms/BinarySearch/BinarySearch.kt
@@ -9,13 +9,12 @@ fun find(arr: Array<Int>, number : Int): Int {
 
 fun findBinary(arr: Array<Int>, number : Int) : Int {
 
-	arr.sort()
 
 	var min = 0;
 	var max = arr.size-1
 	var mid = arr.size/2
 
-	while ((max-min)>2 && arr[mid]!=number) {
+	while (min<=max) {
 	
 		when(number) {
 			arr[min] -> return min
@@ -23,14 +22,12 @@ fun findBinary(arr: Array<Int>, number : Int) : Int {
 			arr[mid] -> return mid
 			else -> {
 				if (number > arr[mid]) {
-					min = mid
-					max = arr.size-1
+					min = mid+1
 					mid = min + (max - min)/2 
 				}
 				else {
-					min = 0
-					max = mid
-					mid = (max - min)/2
+					max = mid-1
+					mid = min + (max - min)/2
 				}
 			}
 		}
@@ -48,7 +45,8 @@ fun findBinary(arr: Array<Int>, number : Int) : Int {
 
 fun main(args: Array<String>) {
 	
-	var arr = arrayOf(1,4,2,13,11,34,5)
-	println("Index is ${findBinary(arr, 4)}")
+	var arr = arrayOf(1,2,4,56,12,3,1,22)
+	arr.sort()
+	println("Index is ${findBinary(arr, 56)}")
 
 }
